4a9d8a6ba9c85b4cab4ec7472227c416c22aac62
FirasJaballi <firas.jaballi@esprit.tn>
2024-04-11 11:56:55 +0100

    making io more partable

--- source/io.c
+++ source/io.c
@@ -1,16 +1,17 @@
 #include <stdlib.h>
+#include <stdio.h>
 #include <fcntl.h>
 #include <unistd.h>
 #include <sys/stat.h>
 #include <io.h>
 
 size_t read_from_file(char *file_name, char **buff) {
-  struct stat stats;
-  stat(file_name, &stats);
-  size_t size=stats.st_size;
+  FILE *fd=fopen(file_name, "rb");
+  fseek(fd, 0L, SEEK_END);
+  size_t size=ftell(fd);
   *buff=malloc(sizeof(char)*size);
-  int fd=open(file_name, O_RDONLY);
-  size_t bytes_read=read(fd, *buff, size);
-  close(fd);
-  return bytes_read;
+  rewind(fd);
+  size_t bytes_read=fread(*buff, size, 1, fd);
+  fclose(fd);
+  return size;
 }
