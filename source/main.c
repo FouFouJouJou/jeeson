061cbfcf7dd1b1e7452daf00e9f29d8db21d0c01
FouFou <firasjaballftw@gmail.com>
2025-05-17 22:48:55 +0100

    adding documentation

--- source/main.c
+++ source/main.c
@@ -11,7 +11,8 @@ int main(int argc, char **args) {
     exit(80);
   }
   struct json_object_t *object=json_to_object(args[1]);
-  struct json_array_t *array = get_array(object, "items");
+  printf_object(*object, 0);
+
   free_json_object(object);
   return EXIT_SUCCESS;
 }
