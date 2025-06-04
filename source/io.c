4543b7c20e047c799f80234c11c684462ac5fbc9
FouFou <firasjaballftw@gmail.com>
2025-06-04 20:56:44 +0100

    fixing io bug

--- source/io.c
+++ source/io.c
@@ -11,9 +11,11 @@ size_t read_from_file(char *file_name, char **buff) {
   size_t size=ftell(fd);
   *buff=malloc(sizeof(char)*size);
   rewind(fd);
-  size_t bytes_read=fread(*buff, size, 1, fd);
+  size_t bytes_read=fread(*buff, sizeof(char), size, fd);
   if (bytes_read != size) {
     fprintf(stderr, "[ERROR]: file was not read fully!");
+    fclose(fd);
+    exit(6);
   }
   fclose(fd);
   return size;
